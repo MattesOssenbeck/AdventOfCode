72d0847b4d06e497ef1f06eb872e5a2e7c6b6c15
Mattes <github@mattes-ossenbeck.de>
2023-12-14 14:20:02 +0100

    Refactor day 07

--- 2023/java/src/main/java/de/ossenbeck/mattes/day07/HandType.java
+++ 2023/java/src/main/java/de/ossenbeck/mattes/day07/HandType.java
@@ -14,21 +14,21 @@ public enum HandType {
     FOUR_OF_A_KIND,
     FIVE_OF_A_KIND;
 
-    private static final Map<Integer, HandType> MAPPINGS = Map.of(
-            50, FIVE_OF_A_KIND,
-            41, FOUR_OF_A_KIND,
-            32, FULL_HOUSE,
-            31, THREE_OF_A_KIND,
-            22, TWO_PAIR,
-            21, ONE_PAIR
+    private static final Map<List<Integer>, HandType> MAPPINGS = Map.of(
+            List.of(5), FIVE_OF_A_KIND,
+            List.of(4, 1), FOUR_OF_A_KIND,
+            List.of(3, 2), FULL_HOUSE,
+            List.of(3, 1, 1), THREE_OF_A_KIND,
+            List.of(2, 2, 1), TWO_PAIR,
+            List.of(2, 1, 1, 1), ONE_PAIR
     );
 
     public static HandType of(List<CamelCard> cards, boolean playingWithJoker) {
         var occurrences = cards.stream().collect(Collectors.toMap(card -> card, initialOccurrence -> 1, Integer::sum));
-        var numberOfJokers = playingWithJoker && occurrences.containsKey(CamelCard.J) ? occurrences.remove(CamelCard.J) : 0;
-        var sortedOccurences = occurrences.values().stream().sorted(Comparator.reverseOrder()).toList();
-        var key = (sortedOccurences.isEmpty() ? numberOfJokers : sortedOccurences.getFirst() + numberOfJokers) * 10;
-        key += sortedOccurences.size() > 1 ? sortedOccurences.get(1) : 0;
-        return MAPPINGS.getOrDefault(key, HIGH_CARD);
+        var numberOfJokers = playingWithJoker && occurrences.containsKey(CamelCard.J) && occurrences.size() > 1
+                ? occurrences.remove(CamelCard.J) : 0;
+        var sortedOccurences = occurrences.values().stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        sortedOccurences.set(0, sortedOccurences.getFirst() + numberOfJokers);
+        return MAPPINGS.getOrDefault(sortedOccurences, HIGH_CARD);
     }
 }
