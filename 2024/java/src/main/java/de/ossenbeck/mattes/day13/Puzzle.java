c715e17c1303ceac7962dd15aeb76f438cebd156
Mattes Ossenbeck <mattes.ossenbeck@web.de>
2024-12-13 18:25:02 +0100

    Refactor day 13

--- 2024/java/src/main/java/de/ossenbeck/mattes/day13/Puzzle.java
+++ 2024/java/src/main/java/de/ossenbeck/mattes/day13/Puzzle.java
@@ -3,27 +3,24 @@ package de.ossenbeck.mattes.day13;
 import de.ossenbeck.mattes.InputReader;
 import de.ossenbeck.mattes.Solvable;
 import de.ossenbeck.mattes.common.Coordinate;
+import de.ossenbeck.mattes.common.Util;
 
 import java.util.List;
 
-import static de.ossenbeck.mattes.common.Util.*;
+import static de.ossenbeck.mattes.common.Util.DOUBLE_LINE_SEPARATOR;
+import static de.ossenbeck.mattes.common.Util.LINE_SEPARATOR;
 
 public class Puzzle implements Solvable<Long, Long> {
     private final List<ClawMachine> clawMachines;
 
     public Puzzle(InputReader inputReader) {
         this.clawMachines = DOUBLE_LINE_SEPARATOR.splitAsStream(inputReader.asString())
-                .map(LINE_SEPARATOR::split)
-                .map(machineBehavior -> {
-                    var btnA = parseNumbers(machineBehavior[0]);
-                    var btnB = parseNumbers(machineBehavior[1]);
-                    var prize = parseNumbers(machineBehavior[2]);
-                    return new ClawMachine(
-                            new Coordinate(btnA.getFirst(), btnA.getLast()),
-                            new Coordinate(btnB.getFirst(), btnB.getLast()),
-                            new Coordinate(prize.getFirst(), prize.getLast())
-                    );
-                }).toList();
+                .map(line -> LINE_SEPARATOR.splitAsStream(line)
+                        .map(Util::parseNumbers)
+                        .map(n -> new Coordinate(n.getFirst(), n.getLast()))
+                        .toList())
+                .map(c -> new ClawMachine(c.getFirst(), c.get(1), c.getLast()))
+                .toList();
     }
 
     @Override
